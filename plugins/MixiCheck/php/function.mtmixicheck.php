85263ca54b3c573cd897c2592d2745f58cafb717
jkaneko <kaneko@gmail.com>
2010-09-14 11:55:49 +0900

    Bug fix in dynamic publishing

--- plugins/MixiCheck/php/function.mtmixicheck.php
+++ plugins/MixiCheck/php/function.mtmixicheck.php
@@ -1,12 +1,12 @@
 <?
 function smarty_function_mtmixicheck($args, &$ctx) {
 
-  $blog  = $ctx->stash('blog');
+  $blog   = $ctx->stash('blog');
   if (!$blog) { return $ctx->error("Error : No blog");}
-  $id    = $ctx->stash('blog_id');
+  $id     = $ctx->stash('blog_id');
   $config = $ctx->mt->db()->fetch_plugin_data('MixiCheck', "configuration:blog:$id");
-  $entry = $ctx->stash('entry');
-  $url = smarty_modifier_encode_html(smarty_function_mtentrypermalink($args, $ctx));
+  $entry  = $ctx->stash('entry');
+  $url    = smarty_modifier_encode_html(smarty_function_mtentrypermalink($args, $ctx));
 
   $button  = '<a href="http://mixi.jp/share.pl" class="mixi-check-button" ' 
     . ' data-key="' . $config['mixi_check_key']  . '" '
--- plugins/MixiCheck/php/function.mtmixiopengraphmeta.php
+++ plugins/MixiCheck/php/function.mtmixiopengraphmeta.php
@@ -2,31 +2,31 @@
 
 function smarty_function_mtmixiopengraphmeta($args, &$ctx) {
 
-  $blog  = $ctx->stash('blog');
+  $blog   = $ctx->stash('blog');
   if (!$blog) { return $ctx->error("Error : No blog");}
-  $id    = $ctx->stash('blog_id');
+  $id     = $ctx->stash('blog_id');
   $config = $ctx->mt->db()->fetch_plugin_data('MixiCheck', "configuration:blog:$id");
-  $entry = $ctx->stash('entry');
-  
-  $url       = $entry ? $entry->permalink : $blog->site_url;
-  $title     = $entry ? $entry->title : $blog->name;
-  $description = $blog->description;
-  
-    
+  $entry  = $ctx->stash('entry');
   
   if ($entry) {
-    $description = $config['mixi_excerpt'] ? $entry->excerpt : $entry->text;                
-  } 
-  $rating = $config['mixi_adult'] ? "1" : "0";
-
+    $url         = smarty_function_mtentrypermalink($args, $ctx);
+    $title       = $entry->title;
+    $description = $config['mixi_excerpt'] ? $entry->excerpt : $entry->text;
+  } else {
+    $url         = smarty_function_mtblogurl($args, $ctx);
+    $title       = $blog->name;
+    $description = $blog->description;
+  }
   $title  = strip_tags($title);
-  $description  = strip_tags($description);
-  $description  = mb_substr($description,0,150);
+  $description   = strip_tags($description);
+  $description   = mb_substr($description,0,150);
+  $rating = $config['mixi_adult'] ? "1" : "0";
     
   $meta = <<<EOT
 <meta property="og:type" content="article"/>
 <meta property="og:title" content="$title" />
 <meta property="og:description" content="$description" />
+<meta property="og:url" content="$url"/> 
 <meta property="mixi:content-rating" content="$rating" />
 EOT;
   return $meta;   
